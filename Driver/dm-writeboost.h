f2708ae1d1a17a7a39cc223633f5a69cf6c54ae5
Akira Hayakawa <ruby.wktk@gmail.com>
2014-01-03 23:10:37 +0900

    Refactor writeboost.h

--- Driver/dm-writeboost.h
+++ Driver/dm-writeboost.h
@@ -28,9 +28,13 @@
  * Nice printk macros
  *
  * Production code should not include lineno
- * but name of the caller is OK.
+ * but name of the caller seems to be OK.
  */
 
+/*
+ * Only for debugging.
+ * Don't include this macro in the production code.
+ */
 #define wbdebug(f, args...) \
 	DMINFO("debug@%s() L.%d" f, __func__, __LINE__, ## args)
 
@@ -44,34 +48,32 @@
 /*----------------------------------------------------------------*/
 
 /*
- * The Detail of the Disk Format
+ * The Detail of the Disk Format (SSD)
+ * -----------------------------------
  *
- * Whole:
+ * ### Overall
  * Superblock (1MB) + Segment + Segment ...
- * We reserve the first 1MB as the superblock.
  *
- * Superblock:
+ * ### Superblock
  * head <----                                     ----> tail
  * superblock header (512B) + ... + superblock record (512B)
  *
- * Segment:
+ * ### Segment
  * segment_header_device (512B) +
  * metablock_device * nr_caches_inseg +
- * (aligned first 4KB region)
- * data[0] (4KB) + data{1] + ... + data{nr_cache_inseg - 1]
+ * data[0] (4KB) + data[1] + ... + data[nr_cache_inseg - 1]
  */
+
+/*----------------------------------------------------------------*/
+
+#define WB_MAGIC 0x57427374 /* Magic number "WBst" */
 
 /*
- * Superblock Header
- * First one sector of the super block region.
- * The value is fixed after formatted.
+ * Superblock Header (Immutable)
+ * -----------------------------
+ * First one sector of the super block region where value
+ * is unchanged after formatted.
  */
-
- /*
-  * Magic Number
-  * "WBst"
-  */
-#define WB_MAGIC 0x57427374
 struct superblock_header_device {
 	__le32 magic;
 	__u8 segment_size_order;
@@ -79,45 +81,19 @@ struct superblock_header_device {
 
 /*
  * Superblock Record (Mutable)
+ * ---------------------------
  * Last one sector of the superblock region.
- * Record the current cache status in need.
+ * Record the current cache status if required.
  */
 struct superblock_record_device {
 	__le64 last_migrated_segment_id;
 } __packed;
 
-/*
- * Metadata of a 4KB cache line
- *
- * Dirtiness is defined for each sector
- * in this cache line.
- */
-struct metablock {
-	sector_t sector; /* key */
-
-	u32 idx; /* Const */
-
-	struct hlist_node ht_list;
-
-	/*
-	 * 8 bit flag for dirtiness
-	 * for each sector in cache line.
-	 *
-	 * Current implementation
-	 * only recovers dirty caches.
-	 * Recovering clean caches complicates the code
-	 * but couldn't be effective
-	 * since only few of the caches are clean.
-	 */
-	u8 dirty_bits;
-};
+/*----------------------------------------------------------------*/
 
 /*
- * On-disk metablock
- *
- * Its size must be a factor of one sector
- * to avoid starddling neighboring two sectors.
- * Facebook's flashcache does the same thing.
+ * The size must be a factor of one sector to avoid starddling
+ * neighboring two sectors. Facebook's flashcache does the same thing.
  */
 struct metablock_device {
 	__le64 sector;
@@ -125,20 +101,54 @@ struct metablock_device {
 	__u8 padding[16 - (8 + 1)]; /* 16B */
 } __packed;
 
+#define WB_CKSUM_SEED (~(u32)0)
+
+struct segment_header_device {
+	/*
+	 * We assume 1 sector write is atomic.
+	 * This 1 sector region contains important information
+	 * such as checksum of the rest of the segment data.
+	 * We use 32bit checksum to audit if the segment is
+	 * correctly written to the cache device.
+	 */
+	/* - FROM ------------------------------------ */
+	__le64 id;
+	/* TODO add timestamp? */
+	__le32 checksum;
+	__u8 length;
+	__u8 padding[512 - (8 + 4 + 1)]; /* 512B */
+	/* - TO -------------------------------------- */
+	struct metablock_device mbarr[0]; /* 16B * N */
+} __packed;
+
+/*----------------------------------------------------------------*/
+
+struct metablock {
+	sector_t sector; /* key */
+
+	u32 idx; /* Const */
+
+	struct hlist_node ht_list;
+
+	u8 dirty_bits; /* 8bit for dirtiness in sector granularity */
+};
+
+/*
+ * (Locking)
+ * Locking metablocks by their granularity needs too much memory
+ * space for lock structures. We only locks a metablock by locking
+ * the parent segment that includes the metablock.
+ */
+#define lockseg(seg, flags) spin_lock_irqsave(&(seg)->lock, flags)
+#define unlockseg(seg, flags) spin_unlock_irqrestore(&(seg)->lock, flags)
+
 #define SZ_MAX (~(size_t)0)
 struct segment_header {
-	/*
-	 * ID uniformly increases.
-	 * ID 0 is used to tell that the segment is invalid
-	 * and valid id >= 1.
-	 */
-	u64 id;
+	u64 id; /* Initialized to 0. */
 
 	/*
-	 * Segment can be flushed half-done.
-	 * length is the number of
-	 * metablocks that must be counted in
-	 * in resuming.
+	 * The count of metablocks in a segment to count in log apply.
+	 * If the RAM buffer is volatile, segment can be less than full length.
 	 */
 	u8 length;
 
@@ -147,19 +157,9 @@ struct segment_header {
 
 	struct list_head migrate_list;
 
-	/*
-	 * This segment can not be migrated
-	 * to backin store
-	 * until flushed.
-	 * Flushed segment is in cache device.
-	 */
-	struct completion flush_done;
+	struct completion flush_done; /* This segment can be migrated */
 
-	/*
-	 * This segment can not be overwritten
-	 * until migrated.
-	 */
-	struct completion migrate_done;
+	struct completion migrate_done; /* This segment can be reused */
 
 	spinlock_t lock;
 
@@ -168,35 +168,37 @@ struct segment_header {
 	struct metablock mb_array[0];
 };
 
-/*
- * (Locking)
- * Locking metablocks by their granularity
- * needs too much memory space for lock structures.
- * We only locks a metablock by locking the parent segment
- * that includes the metablock.
- */
-#define lockseg(seg, flags) spin_lock_irqsave(&(seg)->lock, flags)
-#define unlockseg(seg, flags) spin_unlock_irqrestore(&(seg)->lock, flags)
+/*----------------------------------------------------------------*/
 
-/*
- * On-disk segment header.
- *
- * Must be at most 4KB large.
- */
-#define WB_CKSUM_SEED (~(u32)0)
-struct segment_header_device {
-	/* - FROM ------------------------------------ */
-	__le64 id;
-	__le32 checksum;
-	__u8 length;
-	__u8 padding[512 - (8 + 4 + 1)]; /* 512B */
-	/* - TO -------------------------------------- */
-	struct metablock_device mbarr[0]; /* 16B * N */
-} __packed;
+enum RAMBUF_TYPE {
+	BUF_NORMAL = 0, /* Volatile DRAM */
+	BUF_NV_BLK, /* Non-volatile with block I/F */
+	BUF_NV_RAM, /* Non-volatile with PRAM I/F */
+};
 
 struct rambuffer {
 	void *data;
 	struct completion done;
+};
+
+struct flush_job {
+	struct work_struct work;
+	struct wb_device *wb;
+	struct segment_header *seg;
+	struct rambuffer *rambuf; /* RAM buffer to flush */
+	struct bio_list barrier_ios; /* List of deferred bios */
+};
+
+struct per_bio_data {
+	void *ptr;
+};
+
+struct lookup_key {
+	sector_t sector;
+};
+
+struct ht_head {
+	struct hlist_head ht_list;
 };
 
 enum STATFLAG {
@@ -207,188 +209,173 @@ enum STATFLAG {
 };
 #define STATLEN (1 << 4)
 
-struct lookup_key {
-	sector_t sector;
-};
-
-struct ht_head {
-	struct hlist_head ht_list;
-};
-
 enum WB_FLAG {
 	/*
-	 * The flag WB_DEAD set makes all the daemons
-	 * sleep and device return EIO on any requests.
-	 *
-	 * This flag is set when either one of the underlying
-	 * devices returned EIO and we must immediately
-	 * block up the whole to avoid further damage.
+	 * This flag is set when either one of the underlying devices
+	 * returned EIO and we must immediately block up the whole to
+	 * avoid further damage.
 	 */
 	WB_DEAD = 0,
 };
 
-enum RAMBUF_TYPE {
-	BUF_NORMAL = 0, /* volatile DRAM */
-	BUF_NV_BLK, /* non-volatile with block I/F */
-	BUF_NV_RAM, /* non-volatile with PRAM I/F */
-};
-
 struct wb_device {
-	int type;
+	enum RAMBUF_TYPE type;
 
 	struct dm_target *ti;
 
+	struct dm_dev *origin_dev; /* Slow device (HDD) */
+	struct dm_dev *cache_dev; /* Fast device (SSD) */
+
 	mempool_t *buf_1_pool; /* 1 sector buffer pool */
 	mempool_t *buf_8_pool; /* 8 sector buffer pool */
-	mempool_t *flush_job_pool;
-
-	struct dm_dev *origin_dev;
-	struct dm_dev *cache_dev;
 
 	struct mutex io_lock;
 
 	u8 segment_size_order; /* Const */
 	u8 nr_caches_inseg; /* Const */
 
+	/*---------------------------------------------*/
+
+	/******************
+	 * Current position
+	 ******************/
+
 	/*
-	 * Chained hashtable
-	 *
-	 * Writeboost uses chained hashtable
-	 * to cache lookup.
-	 * Cache discarding often happedns
-	 * This structure fits our needs.
+	 * Current metablock index
+	 * which is the last place already written
+	 * *not* the position to write hereafter.
 	 */
-	u32 cursor; /* Index that has been written the most lately */
+	u32 cursor;
+	struct segment_header *current_seg;
+	struct rambuffer *current_rambuf;
+
+	/*---------------------------------------------*/
+
+	u32 nr_segments; /* Const */
+	struct large_array *segment_header_array;
+
+	/*---------------------------------------------*/
+
+	/********************
+	 * Chained Hash Table
+	 * for cache lookup
+	 ********************/
+
 	u32 nr_caches; /* Const */
 	struct large_array *htable;
 	size_t htsize;
 	struct ht_head *null_head;
 
-	struct segment_header *current_seg;
-	u32 nr_segments; /* Const */
-	struct large_array *segment_header_array;
+	/*---------------------------------------------*/
 
-	struct rambuffer *current_rambuf;
+	mempool_t *flush_job_pool;
 	u32 rambuf_pool_amount; /* kB */
 	u32 nr_rambuf_pool; /* Const */
 	struct rambuffer *rambuf_pool;
 
-	atomic64_t last_migrated_segment_id;
-	atomic64_t last_flushed_segment_id;
+	/*---------------------------------------------*/
 
-	/*
-	 * Flush daemon
-	 *
-	 * Writeboost first queue the segment to flush
-	 * and flush daemon asynchronously
-	 * flush them to the cache device.
-	 */
+	/***********
+	 * wbflusher
+	 ***********/
+
 	struct workqueue_struct *flusher_wq;
 	wait_queue_head_t flush_wait_queue;
+	atomic64_t last_flushed_segment_id;
 
-	/*
-	 * Deferred ACK for barriers.
-	 */
+	/*---------------------------------------------*/
+
+	/*************************
+	 * Barrier deadline worker
+	 *************************/
+
 	struct work_struct barrier_deadline_work;
 	struct timer_list barrier_deadline_timer;
 	struct bio_list barrier_ios;
 	unsigned long barrier_deadline_ms; /* param */
 
-	/*
-	 * Migration daemon
-	 *
-	 * Migartion also works in background.
-	 *
-	 * If allow_migrate is true,
-	 * migrate daemon goes into migration
-	 * if they are segments to migrate.
-	 */
+	/*---------------------------------------------*/
+
+	/****************
+	 * Migrate daemon
+	 ****************/
+
 	struct task_struct *migrate_daemon;
 	int allow_migrate;
-	int urge_migrate; /* Start migrate immediately */
+	int urge_migrate; /* Start migration immediately */
+	atomic64_t last_migrated_segment_id;
 
-	/*
-	 * Batched Migration
-	 *
-	 * Migration is done atomically
-	 * with number of segments batched.
-	 */
+	/****************************************
+	 * Data structures used by migrate daemon
+	 ****************************************/
+
 	wait_queue_head_t migrate_wait_queue;
 	wait_queue_head_t wait_drop_caches;
 	atomic_t migrate_fail_count;
 	atomic_t migrate_io_count;
-	struct list_head migrate_list;
+	struct list_head migrate_list; /* List of segments to migrate */
 	u8 *dirtiness_snapshot;
 	void *migrate_buffer;
 	u32 nr_cur_batched_migration;
-	u32 nr_max_batched_migration; /* param */
+	u32 nr_max_batched_migration; /* tunable */
 
-	/*
+	/*---------------------------------------------*/
+
+	/*********************
 	 * Migration modulator
-	 *
-	 * This daemon turns on and off
-	 * the migration
-	 * according to the load of backing store.
-	 */
+	 *********************/
+
 	struct task_struct *modulator_daemon;
-	int enable_migration_modulator; /* param */
+	int enable_migration_modulator; /* tunable */
 	u8 migrate_threshold;
 
-	/*
-	 * Superblock Recorder
-	 *
-	 * Update the superblock record
-	 * periodically.
-	 */
-	struct task_struct *recorder_daemon;
-	unsigned long update_record_interval; /* param */
+	/*---------------------------------------------*/
 
-	/*
-	 * Cache Synchronizer
-	 *
-	 * Sync the dirty writes
-	 * periodically.
-	 */
+	/*********************
+	 * Superblock recorder
+	 *********************/
+
+	struct task_struct *recorder_daemon;
+	unsigned long update_record_interval; /* tunable */
+
+	/*---------------------------------------------*/
+
+	/*************
+	 * sync daemon
+	 *************/
+
 	struct task_struct *sync_daemon;
-	unsigned long sync_interval; /* param */
+	unsigned long sync_interval; /* tunable */
+
+	/*---------------------------------------------*/
+
+	/************
+	 * Statistics
+	 ************/
 
 	atomic64_t nr_dirty_caches;
 	atomic64_t stat[STATLEN];
 	atomic64_t count_non_full_flushed;
 
-	bool should_emit_tunables;
+	/*---------------------------------------------*/
+
+	/********************
+	 * For device blockup
+	 ********************/
 
 	wait_queue_head_t dead_wait_queue;
 	unsigned long flags;
-};
 
-struct flush_job {
-	struct work_struct work;
+	/*---------------------------------------------*/
 
-	struct wb_device *wb;
-
-	struct segment_header *seg;
-	/*
-	 * The data to flush to cache device.
-	 */
-	struct rambuffer *rambuf;
-	/*
-	 * List of bios with barrier flags.
-	 */
-	struct bio_list barrier_ios;
-};
-
-struct per_bio_data {
-	void *ptr;
+	bool should_emit_tunables;
 };
 
 /*----------------------------------------------------------------*/
 
 void flush_current_buffer(struct wb_device *);
 void inc_nr_dirty_caches(struct wb_device *);
-void cleanup_mb_if_dirty(struct wb_device *,
-			 struct segment_header *,
-			 struct metablock *);
+void cleanup_mb_if_dirty(struct wb_device *, struct segment_header *, struct metablock *);
 u8 atomic_read_mb_dirtiness(struct segment_header *, struct metablock *);
 void invalidate_previous_cache(struct wb_device *, struct segment_header *,
 			       struct metablock *old_mb, bool overwrite_fullsize);
@@ -398,35 +385,29 @@ void invalidate_previous_cache(struct wb_device *, struct segment_header *,
 extern struct workqueue_struct *safe_io_wq;
 extern struct dm_io_client *wb_io_client;
 
-int dm_safe_io_internal(
-		struct wb_device *,
-		struct dm_io_request *,
-		unsigned num_regions, struct dm_io_region *,
-		unsigned long *err_bits, bool thread, const char *caller);
+/*
+ * Wrapper of dm_io function.
+ * Set thread to true to run dm_io in other thread to avoid potential deadlock.
+ */
 #define dm_safe_io(io_req, num_regions, regions, err_bits, thread) \
-		dm_safe_io_internal(wb, (io_req), (num_regions), (regions), \
-				    (err_bits), (thread), __func__);
+	dm_safe_io_internal(wb, (io_req), (num_regions), (regions), \
+			    (err_bits), (thread), __func__);
+int dm_safe_io_internal(struct wb_device *, struct dm_io_request *,
+			unsigned num_regions, struct dm_io_region *,
+			unsigned long *err_bits, bool thread, const char *caller);
 
 sector_t dm_devsize(struct dm_dev *);
 
 /*----------------------------------------------------------------*/
 
-/* Device Blockup */
-
 /*
- * I/O error on either backing or cache
- * should block up the whole system immediately.
- * Either reading or writing a device
- * should not be done if it once returns -EIO.
- * These devices are all untrustable.
- */
-
-/*
- * After the system is blocked up
- * all I/Os to underlying devices are all ignored.
- * IOWs, this is equivalent to having /dev/null devices
- * as these devices and runnig on memory data structures
- * goes on agnostic to the fact that these devices freezed.
+ * Device blockup
+ * --------------
+ *
+ * I/O error on either backing device or cache device should block
+ * up the whole system immediately.
+ * After the system is blocked up all the I/Os to underlying
+ * devices are all ignored as if they are switched to /dev/null.
  */
 
 #define LIVE_DEAD(proc_live, proc_dead) \
@@ -453,17 +434,17 @@ sector_t dm_devsize(struct dm_dev *);
 	} while (0)
 
 /*
- * Only -EIO retuned is regareded as a signal
- * to block up the whole system.
+ * Macro to add context of failure to I/O routine call.
+ * We inherited the idea from Maybe monad of the Haskell language.
  *
- * -EOPNOTSUPP could be retuned if the target device is
- * a virtual device and we request discard.
- *
- * -ENOMEM could be returned from blkdev_issue_discard (3.12-rc5)
- * for example. Wating for a while can make room for alocation.
- *
- * For other unknown error codes we ignore them and
- * ask the human users to report us.
+ * Policies
+ * --------
+ * 1. Only -EIO will block up the system.
+ * 2. -EOPNOTSUPP could be retuned if the target device is a virtual
+ *    device and we request discard.
+ * 3. -ENOMEM could be returned from blkdev_issue_discard (3.12-rc5)
+ *    for example. Waiting for a while can make room for new allocation.
+ * 4. For other unknown error codes we ignore them and ask the users to report.
  */
 #define IO(proc) \
 	do { \

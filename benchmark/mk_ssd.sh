f1bc0d6a68955c4aa79fd60125bfa065c170f2b4
Akira Hayakawa <ruby.wktk@gmail.com>
2014-03-15 10:24:56 +0900

    update configs files
    Signed-off-by: Akira Hayakawa <ruby.wktk@gmail.com>

--- benchmark/mk_ssd.sh
+++ benchmark/mk_ssd.sh
@@ -1,2 +1,3 @@
 . ./config
 /usr/local/util-linux/sbin/blkdiscard --offset 0 --length `blockdev --getsize64 ${CACHE}` $CACHE
+/usr/local/util-linux/sbin/blkdiscard --offset 0 --length `blockdev --getsize64 ${PLOG}` $PLOG
--- prepare.sh
+++ prepare.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 
-T=$1
-
 modprobe libcrc32c
 insmod src/dm-writeboost.ko
 
@@ -19,11 +17,8 @@ dd if=/dev/zero of=${CACHE} bs=512 count=1 oflag=direct
 
 echo create wb device
 sz=`blockdev --getsize ${BACKING}`
-if [ $T -eq 0 ]; then
-    dmsetup create writeboost-vol --table "0 ${sz} writeboost 0 ${BACKING} ${CACHE} 4 segment_size_order 10 nr_rambuf_pool 8 8 enable_migration_modulator 0 allow_migrate 0 sync_interval 0 update_record_interval 0"
-elif [ $T -eq 1 ]; then
-    dmsetup create writeboost-vol --table "0 ${sz} writeboost 1 ${BACKING} ${CACHE} ${PLOG} 4 segment_size_order 10 nr_rambuf_pool 8 8 enable_migration_modulator 0 allow_migrate 0 sync_interval 0 update_record_interval 0"
-fi
+dmsetup create writeboost-vol --table "0 ${sz} writeboost 0 ${BACKING} ${CACHE} 4 segment_size_order 10 nr_rambuf_pool 8 8 enable_migration_modulator 0 allow_migrate 0 sync_interval 0 update_record_interval 0"
+# dmsetup create writeboost-vol --table "0 ${sz} writeboost 1 ${BACKING} ${CACHE} ${PLOG} 4 segment_size_order 10 nr_rambuf_pool 8 8 enable_migration_modulator 0 allow_migrate 0 sync_interval 0 update_record_interval 0"
 
 if [ $? -ne 0 ]; then
     echo "initialization failed. see dmseg"

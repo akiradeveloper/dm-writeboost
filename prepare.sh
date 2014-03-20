a39a9f25b325acf5efb4148b600782298068cbda
Akira Hayakawa <ruby.wktk@gmail.com>
2014-03-21 07:44:34 +0900

    add comment in prepare.sh
    Signed-off-by: Akira Hayakawa <ruby.wktk@gmail.com>

--- prepare.sh
+++ prepare.sh
@@ -19,8 +19,10 @@ echo destroy ...
 dd if=/dev/zero of=${CACHE} bs=512 count=1 oflag=direct
 
 echo create wb device
+# type = 0
 sz=`blockdev --getsize ${BACKING}`
 dmsetup create writeboost-vol --table "0 ${sz} writeboost 0 ${BACKING} ${CACHE} 4 segment_size_order 10 nr_rambuf_pool 8 8 enable_migration_modulator 0 allow_migrate 0 sync_interval 0 update_record_interval 0"
+# type = 1
 # dmsetup create writeboost-vol --table "0 ${sz} writeboost 1 ${BACKING} ${CACHE} ${PLOG} 4 segment_size_order 10 nr_rambuf_pool 8 8 enable_migration_modulator 0 allow_migrate 0 sync_interval 0 update_record_interval 0"
 
 if [ $? -ne 0 ]; then

1e3f1461996c7ebd84539407d743a0dd4175bd0c
Satoru Takeuchi <satoru.takeuchi@gmail.com>
2014-03-30 21:19:09 +0900

    Call blkdiscard iff it exists
    Since dm-writeboost works correctly without calling blkdiscard,
    call it iff it exist for suppressing the unnecessary error message.
    
    Signed-off-by: Satoru Takeuchi <satoru.takeuchi@gmail.com>

--- cleanup-cache.sh
+++ cleanup-cache.sh
@@ -5,9 +5,11 @@
 
 dev=$1
 
-# discard the whole cache device before formatting blkdiscard command is included in upstream util-linux.
-# but don't worry, without discarding, dm-writeboost works correctly.
-blkdiscard --offset 0 --length `blockdev --getsize64 ${dev}` ${dev}
-
+# discard the whole cache device before formatting blkdiscard command is
+# included in upstream util-linux. But don't worry, without discarding,
+# dm-writeboost works correctly.
+if which blkdiscard >/dev/null 2>&1 ; then
+    blkdiscard --offset 0 --length `blockdev --getsize64 ${dev}` ${dev}
+fi
 # zeroing the first sector in the cache device triggers formatting the cache device
 dd if=/dev/zero of=$1 bs=512 count=1 oflag=direct

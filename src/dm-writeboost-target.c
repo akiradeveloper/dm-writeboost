6aad0e7ba2eface011e310879a4b4d0e5ac0c5e9
Akira Hayakawa <ruby.wktk@gmail.com>
2015-08-02 20:19:28 +0900

    v2.1.0
    Signed-off-by: Akira Hayakawa <ruby.wktk@gmail.com>

--- src/dm-writeboost-target.c
+++ src/dm-writeboost-target.c
@@ -1784,7 +1784,7 @@ static void writeboost_status(struct dm_target *ti, status_type_t type,
 
 static struct target_type writeboost_target = {
 	.name = "writeboost",
-	.version = {2, 0, 6},
+	.version = {2, 1, 0},
 	.module = THIS_MODULE,
 	.map = writeboost_map,
 	.end_io = writeboost_end_io,

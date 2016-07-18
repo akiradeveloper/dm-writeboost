28b9e8de938dfd2990bbd03c6773cb36922a8bf2
Akira Hayakawa <ruby.wktk@gmail.com>
2016-07-18 17:09:27 +0900

    close #117 v2.2.1
    Signed-off-by: Akira Hayakawa <ruby.wktk@gmail.com>

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
-MODULE_VERSION ?= 2.2.0
+MODULE_VERSION ?= 2.2.1
 DKMS_DIR := /usr/src/dm-writeboost-$(MODULE_VERSION)
 DKMS_KEY := -m dm-writeboost -v $(MODULE_VERSION)
 
--- src/dm-writeboost-target.c
+++ src/dm-writeboost-target.c
@@ -1853,7 +1853,7 @@ static void writeboost_status(struct dm_target *ti, status_type_t type,
 
 static struct target_type writeboost_target = {
 	.name = "writeboost",
-	.version = {2, 2, 0},
+	.version = {2, 2, 1},
 	.module = THIS_MODULE,
 	.map = writeboost_map,
 	.end_io = writeboost_end_io,

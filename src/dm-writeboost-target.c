337f6ed8e8b966d409a4008c6fff546fbfc0650a
Akira Hayakawa <ruby.wktk@gmail.com>
2016-09-09 08:15:46 +0900

    bump up the version to 2.2.5
    Signed-off-by: Akira Hayakawa <ruby.wktk@gmail.com>

--- src/dm-writeboost-target.c
+++ src/dm-writeboost-target.c
@@ -1874,7 +1874,7 @@ static void writeboost_status(struct dm_target *ti, status_type_t type,
 
 static struct target_type writeboost_target = {
 	.name = "writeboost",
-	.version = {2, 2, 4},
+	.version = {2, 2, 5},
 	.module = THIS_MODULE,
 	.map = writeboost_map,
 	.end_io = writeboost_end_io,

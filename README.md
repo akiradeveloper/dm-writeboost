6b1d0f87568c9e756410e1aee8f88dda1eff34a0
Akira Hayakawa <ruby.wktk@gmail.com>
2017-04-09 13:58:53 +0900

    close #167 update copyright year
    Signed-off-by: Akira Hayakawa <ruby.wktk@gmail.com>

--- README.md
+++ README.md
@@ -61,7 +61,7 @@ Awarded by Japanese OSS Encouragement Award. Thanks!
 
 ## License
 ```
-Copyright (C) 2012-2016 Akira Hayakawa <ruby.wktk@gmail.com>
+Copyright (C) 2012-2017 Akira Hayakawa <ruby.wktk@gmail.com>
 
 This program is free software; you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
--- src/dm-writeboost-target.c
+++ src/dm-writeboost-target.c
@@ -3,7 +3,7 @@
  * Log-structured Caching for Linux
  *
  * This file is part of dm-writeboost
- * Copyright (C) 2012-2016 Akira Hayakawa <ruby.wktk@gmail.com>
+ * Copyright (C) 2012-2017 Akira Hayakawa <ruby.wktk@gmail.com>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by

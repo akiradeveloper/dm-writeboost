bd302661ba642c540905c99749967d454578cd08
Akira Hayakawa <ruby.wktk@gmail.com>
2023-02-11 16:12:11 +0900

    Update copyright year
    Signed-off-by: Akira Hayakawa <ruby.wktk@gmail.com>

--- src/dm-writeboost-daemon.c
+++ src/dm-writeboost-daemon.c
@@ -1,6 +1,6 @@
 /*
  * This file is part of dm-writeboost
- * Copyright (C) 2012-2022 Akira Hayakawa <ruby.wktk@gmail.com>
+ * Copyright (C) 2012-2023 Akira Hayakawa <ruby.wktk@gmail.com>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
